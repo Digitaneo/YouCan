2cabc04efcb79642b6b1e75ecbb2d266b416dc94
Digitaneo <152671435+Digitaneo@users.noreply.github.com>
2025-01-15 14:49:31 +0100

    youcan_price_format.js

--- JS/youcan_price_format.js
+++ JS/youcan_price_format.js
@@ -1,10 +1,28 @@
 <script>
-  const values = document.querySelectorAll('.value');
+  function formatPrices() {
+    const values = document.querySelectorAll('.value');
+    values.forEach(element => {
+      const number = parseInt(element.textContent.replace(/,/g, ''), 10);
+      if (!isNaN(number)) {
+        element.textContent = number.toLocaleString();
+      }
+    });
+  }
 
-  values.forEach(element => {
-    const number = parseInt(element.textContent, 10);
-    if (!isNaN(number)) {
-      element.textContent = number.toLocaleString();
-    }
+  formatPrices();
+
+  const radioButtons = document.querySelectorAll('input[type="radio"]');
+  radioButtons.forEach(radio => {
+    radio.addEventListener('change', () => {
+
+      const priceElement = document.querySelector('.value');
+      if (priceElement) {
+
+        const selectedValue = radio.value;
+        const newPrice = selectedValue == '100' ? 1000 : 1500;
+        priceElement.textContent = newPrice;
+        formatPrices();
+      }
+    });
   });
 </script>
